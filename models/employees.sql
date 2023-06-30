bd17dfd2fc0b5aa9ef33d6e4ddb2f456cb744d76
Raphael Barros <rapha.wb@hotmail.com>
2023-06-30 19:39:56 +0000

    version 1.0

---
+++ models/employees.sql
@@ -0,0 +1,9 @@
+with calc_employees as (
+    select  
+    date_part(year, current_date) - date_part(year, birth_date) as age, -- subtrai ano atual pelo ano de nascimento extraído
+    date_part(year, current_date) - date_part(year, hire_date) as lengthofservice,
+    first_name || ' ' || last_name as name,
+    *
+    from {{source('sources','employees')}}
+)
+select * from calc_employees
